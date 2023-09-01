82f85b42d75e5f2163e89b9e2cd4cdb864165fd8
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2023-09-01 18:37:00 +0400

    Basic skeleton for the 3-frame UI

--- ui.py
+++ ui.py
@@ -1,13 +1,15 @@
-from tkinter import *
-import customtkinter
-from PIL import Image, ImageTk
+import customtkinter as ctk
+from PIL import Image
+from json import load as json_load
 
-customtkinter.set_appearance_mode("Dark")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+ctk.set_appearance_mode("Dark")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
 # Initializing lists
 cart = []
 money = [0]
+
+RESTURAUNT_DATA = json_load(open("data.json", "r"))
 
 # Getting the screen size so the code would work with different devices.
 def get_screen_size():
@@ -15,68 +17,54 @@ def get_screen_size():
     print(f'{width}x{height}')
     return f'{width}x{height}'
 
-# Making the function of the button
-def add_item(name, price):
-    x = ""
-    money[0] = (int(str(money[0])) + price)
-    cart.append(name)
-    for item in set(cart):
-        x += f'{item}      {cart.count(item)}'
-        x += "\n"
-    orders.configure(text=x + str(money[0]))
-    #print(int(money[0]))
-
 # Making the images all of the same size to add them to the button
-def resizing_function(path):
+def resize_image(path, size):
     img = Image.open(path)
-    img = img.resize((200, 200))
+    img = img.resize(size)
     return img
-
 
 # Making the exit button the menu the pops up when pressing on it
 def exit_function():
-    w=customtkinter.CTk()
+    w=ctk.CTk()
     w.geometry("375x200")
     def yes():
         w.destroy()
         app.destroy()
     def no():
         w.destroy()
-    label = customtkinter.CTkLabel(w, text="Do you really want to EXIT the app?", fg_color="transparent", font=("Serif fonts", 20))
+    label = ctk.CTkLabel(w, text="Do you really want to EXIT the app?", fg_color="transparent", font=("Serif fonts", 20))
     label.place(x=30, y=30)
-    yes = customtkinter.CTkButton(w, text="Yes", fg_color="Red", command=yes)
-    no = customtkinter.CTkButton(w, text="No", fg_color="Blue", command=no)
+    yes = ctk.CTkButton(w, text="Yes", fg_color="Red", command=yes)
+    no = ctk.CTkButton(w, text="No", fg_color="Blue", command=no)
     yes.place(x=30, y=90)
     no.place(x=180, y=90)
     w.mainloop()
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry(get_screen_size())
 
-#Defining the image
-twister_img = ImageTk.PhotoImage(resizing_function("twister.jpg"))
-burger_img = ImageTk.PhotoImage(resizing_function("burger.png"))
+app = ctk.CTk()  # create CTk window like you do with the Tk window
+app.title("Resturaunt System")
+app.geometry("1024x768")
+app.minsize(1024, 768)
 
+# Categories section takes 75%, selected item takes 12% and the current order takes 12%
+app.columnconfigure(0, weight=75)
+app.columnconfigure((1, 2), weight=12)
 
-# Making the UI
-title = customtkinter.CTkLabel(app, text="Order", fg_color="transparent", font=("Serif fonts", 30), width=200, height=10)
-title.place(x = 900, y=0)
-title2 = customtkinter.CTkLabel(app, text="Order", fg_color="transparent", font=("Serif fonts", 30), width=200, height=10)
-title2.place(x = 1450, y=0)
-orders = customtkinter.CTkLabel(app, text="", fg_color="transparent", font=("Serif fonts", 30))
-orders.place(x =1500, y=30)
-                                
-# Making the buttons
-button = customtkinter.CTkButton(master=app, text="9 Dhs", command=lambda:add_item("Twister", 9), image=twister_img, compound="top")
-button.place(x=120, y=130, anchor=customtkinter.CENTER)
-button = customtkinter.CTkButton(master=app, text="10 Dhs", command=lambda:add_item("Burger", 10), image=burger_img, compound="top")
-button.place(x=350, y=130, anchor=customtkinter.CENTER)
+app.rowconfigure(1, weight=100)
 
-exit_btn = customtkinter.CTkButton(master=app, text="Exit", fg_color="Red", hover_color="#ff7377", command=exit_function)
-exit_btn.place(x=1750, y=950)
+title = ctk.CTkLabel(app, text=RESTURAUNT_DATA["name"], font=("Arial", 64))
+title.grid(row=0, column=0, sticky="nsew", columnspan=3, padx=10, pady=10)
 
+# CATEGORIES/ITEMS SECTION
+items = ctk.CTkFrame(app)
+items.grid(row=1, column=0, sticky="nsew")
 
-# Running the main loop
+selected = ctk.CTkFrame(app)
+selected.grid(row=1, column=1, sticky="nsew")
+
+order = ctk.CTkFrame(app)
+order.grid(row=1, column=2, sticky="nsew")
+
 app.mainloop()
 
 # Checking what the user bought for debugging purposes
